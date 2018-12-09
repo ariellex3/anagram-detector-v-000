2098d79c05f80eed25e3134a5727e7da6839b1ae
Arielle D <domingu4@gmail.com>
2018-12-09 16:00:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,38 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    array = []
+    anagrams.each do |anagram|
+      letters = anagram.split("").sort
+      if letters == word.split("")
+        array << word
+      else
+        nil
+      end
+    end
+    #takes array of possible anagrams
+    #return all matches in an array
+    #return empty array if no matches
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+# program receives word & list of possible anagrams, selects correct one
